ff71d4266cab6da9297981b5f7f88fbd1f12b05a
Mohammadfarid fakori <fakori.farid@gmail.com>
2024-02-14 11:39:36 -0500

    First Commit

---
+++ Aura/Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Aura/Source/Aura/Public/Character/EnemyCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "EnemyCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacterBase : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
